9755317d0062160a2668b78ee065f87360cb1fd1
Ian King <ian.antking@gmail.com>
2018-11-19 22:59:30 +0000

    added navbar

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import NavBar from './navbar';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,7 +11,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <h1>HawkEye</h1>
+      <NavBar />
     );
   }
 }
