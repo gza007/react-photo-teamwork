e1c06a83ed88f834bd49f8da9a1db2e4dc218eae
Ian King <ian.antking@gmail.com>
2018-11-21 14:26:20 +0000

    added email and password fields to sign-up page, added event handler for onChange

--- src/components/sign-up.jsx
+++ src/components/sign-up.jsx
@@ -4,15 +4,58 @@ class SignUp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: '',
-      password: '',
-      passwordCheck: '',
+      fields: {
+        email: '',
+        password: '',
+      },
     };
   }
 
+  handleFieldChange = (event) => {
+    this.setState({
+      fields: {
+        ...this.state.fields,
+        [event.target.name]: event.target.value,
+      },
+    });
+    console.log(event.target.value);
+  };
+
   render() {
     return (
-      <h1>Sign-up Page</h1>
+      <React.Fragment>
+        <div className="sign-up">
+          <h1>Sign Up</h1>
+          <form>
+            <div className="sign-up_input">
+              <label
+                htmlFor="email"
+              >
+                email:
+              </label>
+              <input
+                name="email"
+                type="email"
+                value={this.state.fields.email}
+                onChange={this.handleFieldChange}
+              />
+            </div>
+            <div className="sign-up_input">
+              <label
+                htmlFor="password"
+              >
+                password:
+              </label>
+              <input
+                name="password"
+                type="password"
+                value={this.state.fields.password}
+                onChange={this.handleFieldChange}
+              />
+            </div>
+          </form>
+        </div>
+      </React.Fragment>
     );
   }
 }
