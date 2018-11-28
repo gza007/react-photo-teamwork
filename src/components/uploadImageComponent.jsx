587405d32dff8d95237348dd0b4d5b7f34ebc49d
Ryan Brockley <rybrockers@yahoo.com>
2018-11-28 19:23:23 +0000

    working on upload-image

--- src/components/uploadImageComponent.jsx
+++ src/components/uploadImageComponent.jsx
@@ -1,56 +1,27 @@
 import React from 'react';
-import '../Styles/uploadImageComponent.css';
-import axios from 'axios';
 
 class UploadImage extends React.Component {
   constructor(props) {
     super(props);
     this.setState = {
-      fields: {
-        src: '',
-        caption: '',
-        tags: [],
-      },
+      file: '',
+      src: '',
     };
   }
 
-  handlenonClick = (event) => {
-    axios.post('https://mcr-codes-image-sharing-api.herokuapp.com/images')
-      .then((response) => {
-
-      });
-  };
-
-  componentDidMount() {
-
-  }
-
-
   render() {
     return (
-      <React.Fragment>
-        <div className="form-header">
-          <h1>Upload An Image</h1>
-        </div>
-        <div className="upload-form">
-          <form>
+      <div className="upload-form">
+        <h1>Upload An Image</h1>
+        <form>
 
-            <div className="caption-div">
-              <label htmlFor="Caption">Add A Caption:</label>
-              <input name="caption" placeholder="Add A Caption" type="text" />
-            </div>
-            <div className="tags-div">
-              <label htmlFor="Tags">Tag Away:</label>
-              <input name="tags" placeholder="Tags" type="text" />
-            </div>
-            <div>
-              <input name="image" type="file" />
-              <button type="submit" className="upload-image-button" onClick={this.handlenonClick}>Upload</button>
-            </div>
-          </form>
-        </div>
-      </React.Fragment>
+          <label htmlFor="Image"></label>
+          <input type="file"></input>
 
+
+        </form>
+
+      </div>
     );
   }
 }
