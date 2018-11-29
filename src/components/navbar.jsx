826138bb891d2eaf0616e9209384c433723c7ff7
Ryan Brockley <rybrockers@yahoo.com>
2018-11-29 22:13:27 +0000

    pulling latest comments codes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faSignInAlt,
   faUserCircle,
--- src/components/uploadImageComponent.jsx
+++ src/components/uploadImageComponent.jsx
@@ -1,25 +1,40 @@
 /* eslint-disable react/self-closing-comp */
 import React from 'react';
 import '../Styles/uploadImageComponent.css';
+import Axios from 'axios';
 
 
 class UploadImage extends React.Component {
   constructor(props) {
     super(props);
     this.setState = {
-      file: '',
-      src: '',
+      selectedFile: null,
     };
   }
+
+  onChange = event => {
+    this.setState({
+      selectedFile: event.target.files[0],
+
+    });
+  };
+
+  onClick = () => {
+    const formData = new FormData();
+    formData.append('image', this.state.selectedFile, this.state.selectedFile.name);
+    Axios.post('https://mcr-codes-image-sharing-api.herokuapp.com/images', formData)
+      .then();
+  };
+
 
   render() {
     return (
       <div className="upload-form">
         <form>
-          <input name="upload-file" type="file">
-
-            <button type="submit">Submit</button>
+          <input name="upload-file" type="file" onChange={this.handleonChange}>
           </input>
+
+          <button type="submit" onClick={this.handleonClick}>Submit</button>
         </form>
       </div>
     );
