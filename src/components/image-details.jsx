802850db3b2ff11aa28da4611ed063c70b7ea031
Richard Pentecost <richardpentecost1@gmail.com>
2019-05-02 21:28:37 +0100

    setting up api calls

--- src/components/image-details.jsx
+++ src/components/image-details.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import Image from './image';
 import Comments from './comments';
+import axios from 'axios';
+
+const URL = 'http://mcr-codes-image-sharing-api.herokuapp.com';
 
 class ImageDetails extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      imageId: '',
-      user: 'Richard',
-      src: '../../image/vicious_dog_0.png',
+      imageId: '5c01bcadf4318f00164926fb',
+      user: {},
+      src: '',
       thumb: '',
       caption: '',
-      tags: '',
-      comments: ['damn your aggressive', 'more punk IPA'],
-      timestamp: '',
+      tags: [],
+      comments: [],
+      timestamp: 0,
       likes: 0,
       isLiked: false,
     };
@@ -29,7 +32,26 @@ class ImageDetails extends React.Component {
     this.state.comments.push(comment);
   };
 
-  
+  componentDidMount() {
+    axios.get(`${URL}/images/${this.state.imageId}`)
+      .then(response => {
+        this.setState({
+          user: response.data.user,
+          src: response.data.src,
+          thumb: response.data.thumb,
+          caption: response.data.caption,
+          tags: response.data.tags,
+          comments: response.data.comments,
+          timestamp: response.data.timestamp,
+          likes: response.data.likes,
+          isLiked: response.data.isLiked,
+        });
+        console.log(this.state);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
   render() {
     const {
@@ -45,15 +67,19 @@ class ImageDetails extends React.Component {
       isLiked,
     } = this.state;
 
+    if (!comments) {
+      return <h1>loading...</h1>;
+    };
+
     return (
       <div>
-        <Image src={src} user={user} />
-        <Comments
+        <Image src={src} user={user.firstName} />
+        {/*<Comments
           comments={comments}
           isLiked={isLiked}
           onLike={this.handleLike}
           onSubmit={this.handleCommentSubmit}
-        />
+        />*/}
       </div>
     );
   }
