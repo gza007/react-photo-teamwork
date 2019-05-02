46f76e666f7d5030f966dce93e346cf181321b41
Vitnija Bluzma <thepunkyone@gmail.com>
2019-05-02 21:18:48 +0100

    added style

--- src/components/image-browser.jsx
+++ src/components/image-browser.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import axios from 'axios';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment, faHeart } from '@fortawesome/free-solid-svg-icons';
+import '../css/image-browser.css';
+
+library.add(faComment, faHeart);
 
 const URL = 'http://mcr-codes-image-sharing-api.herokuapp.com/images';
 
@@ -29,20 +35,26 @@ componentDidMount() {
 
 render() {
   return (
-    <div>
-      <div>
+    <div className="image-grid">
         {this.state.images.map(image => {
           return (
-            <div key={image._id}>
-              <img src={image.thumb} />
-              <div>
-                <span>Comments: {image.comments.length}</span>
-                <span>{` Likes: ${image.likes}`}</span>
+            <div key={image._id} className="thumbnail-image">
+              <div className="image-frame">
+                <img src={image.thumb} />
+              </div>
+              <div className="thumbnail-stats">
+                <span>
+                  <FontAwesomeIcon icon="comment" className="icon"/>
+                  {image.comments.length}
+                </span>
+                <span>
+                  <FontAwesomeIcon icon="heart" className="icon"/>
+                  {image.likes}
+                </span>
               </div>
             </div>
           );
         })}
-      </div>
     </div>
   );
 }
